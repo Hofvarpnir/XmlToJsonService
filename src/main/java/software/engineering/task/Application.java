35466c6ee97c11dd6fb331d3d80eda11fb524c79
Nikita Fedoseenkov <nikitaf40@gmail.com>
2020-01-04 20:50:07 +0300

    Update Application.java

--- src/main/java/software/engineering/task/Application.java
+++ src/main/java/software/engineering/task/Application.java
@@ -2,12 +2,19 @@ package software.engineering.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * This is entry point to the program
  */
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
     /**
      * Main method launch server
      *
