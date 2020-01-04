5cf08bd4222024e8c259c1e1565dc8129e22911c
Nikita Fedoseenkov <nikitaf40@gmail.com>
2020-01-04 20:53:02 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "software.engineering.task"
-version = "1.1"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -17,7 +17,14 @@ repositories {
 dependencies {
     compile("org.springframework.boot:spring-boot-starter-web")
     compile("org.json", "json", "20140107")
+    compile("com.github.opendevl", "json2flat", "1.0.3")
     compile("com.google.code.gson:gson:2.8.5")
+    compile("org.apache.httpcomponents:httpclient:4.5.7")
+    compile("org.cryptacular:cryptacular:1.2.3")
+    compile("redis.clients:jedis:2.9.0")
+    compile("org.springframework.data:spring-data-redis:2.0.3.RELEASE")
+    compile("com.itextpdf:itextpdf:5.5.10")
+    compile("org.apache.pdfbox:pdfbox:2.0.4")
     testCompile("junit", "junit", "4.12")
 }
 
@@ -25,19 +32,17 @@ configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
 
-tasks {
-    register("unpack", Copy::class) {
-        dependsOn(tasks.named<BootJar>("bootJar"));
+tasks.register<Copy>("unpack") {
+    dependsOn(tasks.named<BootJar>("bootJar"));
 
-        from(zipTree(tasks.named<BootJar>("bootJar").get().outputs.files.singleFile))
-        into("build/dependency")
-    }
+    from(zipTree(tasks.named<BootJar>("bootJar").get().outputs.files.singleFile))
+    into("build/dependency")
 }
 
 docker {
     name = tasks.named<BootJar>("bootJar").get().baseName
-    copySpec.from(tasks["unpack"].outputs).into("dependency")
-    buildArgs(mapOf("DEPENDENCY" to "dependency"))
+    copySpec.from(tasks["unpack"].outputs).into("dependency2121")
+    buildArgs(mapOf("DEPENDENCY" to "dependency2121"))
 }
 
 tasks["build"].finalizedBy(tasks["docker"])
